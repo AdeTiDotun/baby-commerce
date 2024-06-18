19f431b9850ab31a176f8c0d9fa8a29d151cbd4a
Daniel <adejumodaniel274@gmail.com>
2024-06-18 22:08:51 +0100

    arrange navbar lists to positions

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,17 +1,21 @@
+import "./Navbar.css";
+
 function Navbar() {
   return (
     <>
-      <ul>
-        <li></li>
-        <li>Pregnancy</li>
-        <li>Getting Pregnant</li>
-        <li>Parent</li>
-        <li>Baby & Toddler</li>
-        <li>Our Store</li>
-        <li>Community</li>
-        <li>Login</li>
-        <li>Join</li>
-      </ul>
+      <div>
+        <ul>
+          <li></li>
+          <li className="list1">Pregnancy</li>
+          <li>Getting Pregnant</li>
+          <li>Parent</li>
+          <li>Baby & Toddler</li>
+          <li>Our Store</li>
+          <li>Community</li>
+          <li>Login</li>
+          <li>Join</li>
+        </ul>
+      </div>
     </>
   );
 }
